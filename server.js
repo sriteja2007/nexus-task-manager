e0bb9cab990f317412a2a52900b63896fe9497ad
SriTeja <asriteja2387@gmail.com>
2025-12-05 23:49:47 +0530

    Secure setup with .env

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config(); // <-- Load .env file
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// routes
+// Routes
 const taskRoutes = require("./backend/routes/taskRoutes");
 app.use("/tasks", taskRoutes);
 
-// MongoDB Connection
-mongoose.connect(
-  "mongodb+srv://sriteja_15:SriTeja%402007@cluster0.fw2kd8s.mongodb.net/nexus?retryWrites=true&w=majority&appName=Cluster0"
-)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log("MongoDB Error:", err));
+// Connect to MongoDB using .env variable
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
 
+// Start Server
 app.listen(5000, () => console.log("Server running on port 5000"));
